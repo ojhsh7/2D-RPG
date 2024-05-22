6d03b0e65ed01091d7bd49dee2c3b1b5889f527c
ojhsh7 <163072092+ojhsh7@users.noreply.github.com>
2024-05-22 15:44:33 +0900

    SelectScene

--- Assets/Scripts/SelectCharacter.cs
+++ Assets/Scripts/SelectCharacter.cs
@@ -1,4 +1,6 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Select : MonoBehaviour
@@ -10,9 +12,37 @@ public class Select : MonoBehaviour
 
     [Header("Character")]
     public GameObject[] Characters; //Warrior, Archer, Mage
+    public CreatInfo[] CharacterInfos;
     private int charIndex = 0;
 
+    [Header("GameStart")]
+    public GameObject GameStart;
+    public Text GameCountTxt;
+    private bool isPlayButtonClicked = false;
+    private float gameCount = 5f;
 
+    public static string CharacterName;
+
+    private void Update()
+    {
+        if (isPlayButtonClicked)
+        {
+            gameCount -= Time.deltaTime;
+            if (gameCount <= 0 )
+            {
+                SceneManager.LoadScene("MainScene");
+            }
+            GameCountTxt.text = $"곧 게임이 시작됩니다. \n {gameCount:F1}";
+        }
+    }
+
+
+    public void PlayBtn()
+    {
+        GameStart.SetActive(true);
+        isPlayButtonClicked =true;
+        CharacterName = Characters[charIndex].name;
+    }
     public void SelectCharactBtn(string btnName)
     {
         Characters[charIndex].SetActive(false);
@@ -29,5 +59,20 @@ public class Select : MonoBehaviour
         }
         Debug.Log($"CharIndex : {charIndex}");
         Characters[charIndex].SetActive(true);
+        SetPanelInfo();
+    }
+    private void SetPanelInfo()
+    {
+        NameTxt.text = CharacterInfos[charIndex].Name;
+        Feature.text = CharacterInfos[charIndex].Feature;
+        CharImage.sprite = Characters[charIndex].GetComponent<SpriteRenderer>().sprite;
+    }
+
+    private void Start()
+    {
+        SetPanelInfo();
     }
 }
+
+
+
